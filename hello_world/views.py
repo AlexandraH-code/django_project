3313200c34e40c4a949865d6c305e5bede1a59d4
AlexandraH-code <ro24nz8c@students.codeinstitute.net>
2025-04-15 12:18:48 +0200

    Change the index funtion in hello_world/views.py back to the original state

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    #return HttpResponse("Hello, world!")
+    return HttpResponse("Hello, world!")
 
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+    #if request.method == "POST":
+        #return HttpResponse("You must have POSTed something")
+    #else:
+        #return HttpResponse(request.method)
